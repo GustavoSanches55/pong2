fd121aeb96132f60a555d5d6fad897b4b6e90d42
Lfzinho <laguardialuis@gmail.com>
2021-12-02 18:23:36 -0300

    django-admin startapp pra geral

---
+++ pong/kayo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KayoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kayo'
---
+++ pong/sanches/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SanchesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sanches'
