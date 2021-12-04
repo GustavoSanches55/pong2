0451d169d42b857f8d5d3554a634366890f3f957
Lfzinho <laguardialuis@gmail.com>
2021-12-04 01:32:25 -0300

    adiciona notas

--- pong/menu/urls.py
+++ pong/menu/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("jogo", views.jogo, name="jogo"),
     path("salvar_csv", views.salvar_csv, name="salvar_csv"),
     path("analises_menu", views.analises_menu, name="analises_menu"),
+    path("notas", views.notas, name="notas"),
     path(r'sanches/', include('sanches.urls',  namespace='sanches')),
     path(r'kayo/', include('kayo.urls',  namespace='kayo')),
     path(r'laguardia/', include('laguardia.urls',  namespace='laguardia'))
