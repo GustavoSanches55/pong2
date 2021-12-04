550182c96958c6dcd3ffac818763816c21dacbe6
LucasTreuke <lucasbryantreuke@gmail.com>
2021-12-04 15:56:05 -0300

    Adicionado Rank e tempo de jogo no profile

--- pong/treuke/views.py
+++ pong/treuke/views.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+plt.style.use('default')
 
 def index(request):
     db_values = Jogador.objects.all().values()
